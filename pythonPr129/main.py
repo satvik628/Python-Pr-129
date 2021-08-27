19772043c2fe9c846aeda77876955e57a76098f8
satvik628 <71756957+satvik628@users.noreply.github.com>
2021-08-27 16:52:04 +0530

    complete folder after data merging .

---
+++ pythonPr129/main.py
@@ -0,0 +1,39 @@
+import csv
+
+
+data =[]
+
+
+with open("dwarf_stars.csv","r") as f:
+    csvreader=csv.reader(f)
+
+    for row in csvreader:
+        data.append(row)
+
+
+headers=data[0]
+planet_data=data[1:]
+
+
+
+#Converting all planet names to lower case
+for data_point in planet_data:
+    data_point[2] = data_point[2].lower()
+
+#Sorting planet names in alphabetical order
+planet_data.sort(key=lambda planet_data: planet_data[2])
+
+
+with open("dwarf_stars_sorted.csv", "a+") as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planet_data)
+
+
+
+
+
+
+
+
+
